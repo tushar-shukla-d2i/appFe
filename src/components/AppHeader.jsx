59e234fde0eba28350aabc00a11952f3a8e32e32
divyaguptad2i <divyagupta.d2i@gmail.com>
2024-08-20 14:47:17 +0530

    Redirect to dashboard on click app header icon

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -3,13 +3,23 @@
  */
 
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import { AppRoutes } from "../constants";
 import d2iLogo from "../assets/d2i_logo.jpg";
 
 const AppHeader = () => {
+  const navigate = useNavigate();
+
   return (
     <header className="bg-[#0375a7] text-white py-4">
       <div className="flex items-center justify-center">
-        <img src={d2iLogo} alt="logo" className="rounded-full h-16 w-16 mr-4" />
+        <img
+          src={d2iLogo}
+          alt="logo"
+          className="rounded-full h-16 w-16 mr-4 hover:cursor-pointer"
+          onClick={() => navigate(AppRoutes.DASHBOARD)}
+        />
         <h1 className="text-lg font-bold tracking-wider	">
           Innovating Tomorrow, Today
         </h1>
