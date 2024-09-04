ff5ea8353c0f4278b08f81b760b6ce0b32b5a761
divyaguptad2i <divyagupta.d2i@gmail.com>
2024-09-04 14:26:32 +0530

    Update attendance API to accept user_id and attendanceDate conditionally

--- src/apis/attendanceApis.js
+++ src/apis/attendanceApis.js
@@ -6,11 +6,17 @@ import { endpoints } from "./endpoints";
 import { httpClient } from "../utils/HttpUtils";
 
 export const attendanceApis = {
-  getAttendance: async ({ user_id }) => {
+  getAttendance: async ({ user_id, attendanceDate }) => {
     try {
-      const response = await httpClient.get(
-        `${endpoints.ATTENDANCE}/${user_id}`
-      );
+      let url = endpoints.ATTENDANCE;
+      if (user_id) {
+        url += `/${user_id}`;
+      }
+      const params = {};
+      if (attendanceDate) {
+        params.attendanceDate = attendanceDate;
+      }
+      const response = await httpClient.get(url, { params });
       return response;
     } catch (error) {
       console.log("getAttendance:", error);
