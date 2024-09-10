0842fe3fffd3e15602110118aa512447d801bc4f
divyaguptad2i <divyagupta.d2i@gmail.com>
2024-09-10 13:21:07 +0530

    Update env and vite config

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,13 +1,13 @@
 const Config = {
   DEV: {
-    BE_BASE_URL: "http://localhost:3000/api/v1/",
-    FE_BASE_URL: "http://localhost:5173/",
-    IMAGE_BASE_URL: "http://localhost:3000/api/uploads/",
+    BE_BASE_URL: process.env.REACT_APP_BE_BASE_URL,
+    FE_BASE_URL: process.env.REACT_APP_FE_BASE_URL,
+    IMAGE_BASE_URL: process.env.REACT_APP_IMAGE_BASE_URL,
   },
   LOCAL: {
-    BE_BASE_URL: "http://localhost:3000/api/v1/",
-    FE_BASE_URL: "http://localhost:5173/",
-    IMAGE_BASE_URL: "http://localhost:3000/api/uploads/",
+    BE_BASE_URL: process.env.REACT_APP_BE_BASE_URL,
+    FE_BASE_URL: process.env.REACT_APP_FE_BASE_URL,
+    IMAGE_BASE_URL: process.env.REACT_APP_IMAGE_BASE_URL,
   },
 };
 
