16417a55740668e2b2cce48230ce044a3bdcb339
divyaguptad2i <divyagupta.d2i@gmail.com>
2024-09-12 12:20:20 +0530

    Implement one-signal

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import "./App.css";
@@ -22,6 +22,57 @@ import {
 } from "./pages";
 
 const App = () => {
+  window.OneSignal = window.OneSignal || [];
+  const OneSignal = window.OneSignal;
+
+  useEffect(() => {
+    OneSignal.push(() => {
+      OneSignal.init(
+        {
+          appId: "61699a51-2649-4711-9dec-330d06a7d771",
+          promptOptions: {
+            slidedown: {
+              enabled: true,
+              actionMessage:
+                "We'd like to show you notifications for the latest news and updates about the following categories.",
+              acceptButtonText: "OMG YEEEEESS!",
+              cancelButtonText: "NAHHH",
+              categories: {
+                tags: [
+                  {
+                    tag: "react",
+                    label: "ReactJS",
+                  },
+                  {
+                    tag: "angular",
+                    label: "Angular",
+                  },
+                  {
+                    tag: "vue",
+                    label: "VueJS",
+                  },
+                  {
+                    tag: "js",
+                    label: "JavaScript",
+                  },
+                ],
+              },
+            },
+          },
+          welcomeNotification: {
+            title: "One Signal",
+            message: "Thanks for subscribing!",
+          },
+        },
+        //Automatically subscribe to the new_app_version tag
+        OneSignal.sendTag("new_app_version", "new_app_version", (tagsSent) => {
+          // Callback called when tag has finished sending
+          console.log("new_app_version TAG SENT", tagsSent);
+        })
+      );
+    });
+  }, []);
+
   return (
     <div className="App">
       <AppHeader />
