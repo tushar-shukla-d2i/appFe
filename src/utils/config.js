63a7693feb9a2a622d0c3195d4b3e29c748a99b0
avasthiji <avasthi.d2itechnology@gmail.com>
2024-09-13 15:16:51 +0530

    update BE url

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,9 @@
 const Config = {
   DEV: {
     BE_BASE_URL:
-      "https://b761-2405-201-5c0f-a06c-4d7-f0b5-a355-f3ea.ngrok-free.app/api/v1/",
+      "https://d2i.devlocal.in/",
     IMAGE_BASE_URL:
-      "https://b761-2405-201-5c0f-a06c-4d7-f0b5-a355-f3ea.ngrok-free.app/api/uploads/",
+      "https://d2i.devlocal.in/api/uploads/",
   },
   LOCAL: {
     BE_BASE_URL: "http://localhost:3000/api/v1/",
