7ef39680e704f8d12d0811a9f79b562f49fbb887
divyaguptad2i <divyagupta.d2i@gmail.com>
2024-09-13 17:20:26 +0530

    Remove console

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,7 @@ const App = () => {
         //Automatically subscribe to the new_app_version tag
         OneSignal.sendTag("new_app_version", "new_app_version", (tagsSent) => {
           // Callback called when tag has finished sending
-          console.log("new_app_version TAG SENT", tagsSent);
+          // console.log("new_app_version TAG SENT", tagsSent);
         })
       );
     });
