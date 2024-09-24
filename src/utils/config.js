0859dd128a4ed26016e869ade3834a6a86b6f7c4
divyaguptad2i <divyagupta.d2i@gmail.com>
2024-09-24 22:32:43 +0530

    Change BE base url

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,13 +1,15 @@
 const Config = {
   DEV: {
     BE_BASE_URL:
-      "https://b761-2405-201-5c0f-a06c-4d7-f0b5-a355-f3ea.ngrok-free.app/api/v1/",
+      "https://deb9-2405-201-5c0f-a06c-829-412c-f00e-8ece.ngrok-free.app/api/v1",
     IMAGE_BASE_URL:
-      "https://b761-2405-201-5c0f-a06c-4d7-f0b5-a355-f3ea.ngrok-free.app/api/uploads/",
+      "https://deb9-2405-201-5c0f-a06c-829-412c-f00e-8ece.ngrok-free.app/api/uploads/",
   },
   LOCAL: {
-    BE_BASE_URL: "http://localhost:3000/api/v1/",
-    IMAGE_BASE_URL: "http://localhost:3000/api/uploads/",
+    BE_BASE_URL:
+      "https://deb9-2405-201-5c0f-a06c-829-412c-f00e-8ece.ngrok-free.app/api/v1/",
+    IMAGE_BASE_URL:
+      "https://deb9-2405-201-5c0f-a06c-829-412c-f00e-8ece.ngrok-free.app/api/uploads/",
   },
   ONE_SIGNAL_APP_ID: "61699a51-2649-4711-9dec-330d06a7d771",
 };
