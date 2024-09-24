f963f177e3804c9528d1a682f5f94ea41015a928
divyaguptad2i <divyagupta.d2i@gmail.com>
2024-09-24 22:44:33 +0530

    Revert changes config file

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,15 +1,11 @@
 const Config = {
   DEV: {
-    BE_BASE_URL:
-      "https://deb9-2405-201-5c0f-a06c-829-412c-f00e-8ece.ngrok-free.app/api/v1",
-    IMAGE_BASE_URL:
-      "https://deb9-2405-201-5c0f-a06c-829-412c-f00e-8ece.ngrok-free.app/api/uploads/",
+    BE_BASE_URL: "https://d2i.devlocal.in/api/v1",
+    IMAGE_BASE_URL: "https://d2i.devlocal.in/api/uploads/",
   },
   LOCAL: {
-    BE_BASE_URL:
-      "https://deb9-2405-201-5c0f-a06c-829-412c-f00e-8ece.ngrok-free.app/api/v1/",
-    IMAGE_BASE_URL:
-      "https://deb9-2405-201-5c0f-a06c-829-412c-f00e-8ece.ngrok-free.app/api/uploads/",
+    BE_BASE_URL: "http://localhost:3000/api/v1/",
+    IMAGE_BASE_URL: "http://localhost:3000/api/uploads/",
   },
   ONE_SIGNAL_APP_ID: "61699a51-2649-4711-9dec-330d06a7d771",
 };
